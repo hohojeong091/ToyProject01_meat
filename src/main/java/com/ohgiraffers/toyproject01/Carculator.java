ca24fc0ec1e139ed3b661e371b47bcd2b88396f4
hoje091 <hj660833@gmail.com>
2024-07-03 16:04:24 +0900

    07/03 개발 중..

--- src/main/java/com/ohgiraffers/toyproject01/Carculator.java
+++ src/main/java/com/ohgiraffers/toyproject01/Carculator.java
@@ -3,5 +3,15 @@ package com.ohgiraffers.toyproject01;
 public class Carculator {
 
     private String kind;
-    private
+    private int gram;
+
+    private String partName;
+    private int price;
+
+    //종류, 그램, 부위, 가격
+
+    public void carculateMeat ( String kind, int gram, String partName, int price) {
+
+
+    }
 }
